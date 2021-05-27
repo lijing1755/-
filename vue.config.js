8fc4304ac65ef6ef141c74a80e408df4738ce5ae
chenxing <c_x_hello@163.com>
2021-05-27 14:32:46 +0800

    Update 增加gzip压缩

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
@@ -79,6 +80,35 @@ module.exports = {
       .options({
         symbolId: 'icon-[name]'
       })
+  },
+  configureWebpack: config => {
+    config.plugins.push(new CompressionWebpackPlugin({
+      algorithm: 'gzip',
+      test: /\.js$|\.html$|\.json$|\.css/,
+      threshold: 10240,
+      minRatio: 0.8
+    }))
+    // 开启分离js
+    config.optimization = {
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 20000,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              // get the name. E.g. node_modules/packageName/not/this/part.js
+              // or node_modules/packageName
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+              // npm package names are URL-safe, but some servers don't like @ symbols
+              return `npm.${packageName.replace('@', '')}`
+            }
+          }
+        }
+      }
+    }
   },
   devServer: {
     open: true,
